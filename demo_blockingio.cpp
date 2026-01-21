2b1a62b35bcd006468ca8a3e6dfff5eaea9378ac
Bernd Porr <mail@berndporr.me.uk>
2026-01-21 15:51:13 +0000

    Added blocking I/O in its simplest form.

---
+++ demo_blockingio.cpp
@@ -0,0 +1,17 @@
+/**
+ * GNU GENERAL PUBLIC LICENSE
+ * Version 3, 29 June 2007
+ *
+ * (C) 2020-2025, Bernd Porr <mail@bernporr.me.uk>
+ * 
+ **/
+
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
+
+int main() {
+    printf("Press enter\n");
+    getchar();
+    return 0;
+};
